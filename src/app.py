cbff62de5e2d3065c0493e2f13de398347bde816
Cutleast <cutleast@gmail.com>
2024-03-14 11:55:33 +0100

    Add Commandline Arguments and "help" command

--- src/app.py
+++ src/app.py
@@ -12,14 +12,16 @@ sys.argv.pop(0)
 
 print(f"{sys.argv = }")
 
-if len(sys.argv) >= 2:
-    if sys.argv[0] == "--download" and sys.argv[1].startswith("nxm://"):
-        from nxm_handler import handle
+match sys.argv[0]:
+    case "--download":
+        if sys.argv[-1].startswith("nxm://"):
+            from nxm_handler import handle
 
-        handle()
+            handle()
+        else:
+            print("Missing Download URL!")
 
-elif len(sys.argv) >= 1:
-    if sys.argv[0] == "--bind-nxm":
+    case "--bind-nxm":
         import winreg
 
         from main import MainApp
@@ -35,11 +37,69 @@ elif len(sys.argv) >= 1:
 
         except OSError:
             sys.exit(1)
-    elif sys.argv[0] == "--show-docs":
+
+    case "--show-docs":
         from main import MainApp
 
         app = MainApp()
         app.show_documentation_standalone()
+
+        sys.exit(0)
+
+    case "--extract-strings":
+        from pathlib import Path
+
+        plugin_path = Path(sys.argv[-1])
+
+        if not plugin_path.is_file():
+            print(f"{str(plugin_path)!r} is not an existing file!")
+            sys.exit(1)
+
+        if plugin_path.suffix.lower() in [".esp", ".esl", ".esm"]:
+            import utilities
+            from plugin_parser import PluginParser
+            import json
+
+            output_path = plugin_path.with_suffix(f"{plugin_path.suffix}.json")
+
+            parser = PluginParser(plugin_path)
+            strings = [
+                string.to_string_data()
+                for group in parser.extract_strings().values()
+                for string in group
+            ]
+
+            with output_path.open("w", encoding="utf8") as output_file:
+                json.dump(strings, output_file, indent=4, ensure_ascii=False)
+
+            print(f"Strings written to {str(output_path)!r}.")
+
+            sys.exit(0)
+
+    case "--help":
+        print(
+            "--download <NXM URL>                      Starts download of file at given URL"
+        )
+        print(
+            "--show-docs                               Opens Documentation without starting Main Application."
+        )
+        print(
+            "--bind-nxm                                Binds Mod Manager Downloads to SSE-AT. Mainly used for Admin Rights"
+        )
+        print(
+            "--extract-strings <Path to Plugin File>   Extracts Strings from Plugin and writes them in a JSON file with the same location and name."
+        )
+
+        sys.exit(0)
+
+    case "--version":
+        from main import MainApp
+
+        print(f"{MainApp.name} v{MainApp.version} by Cutleast")
+        print()
+        print(
+            "Licensed under Attribution-NonCommercial-NoDerivatives 4.0 International"
+        )
 
         sys.exit(0)
 
