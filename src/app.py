794b861eba47f4a99c5398ac209d9c5f9aa85b23
Cutleast <cutleast@gmail.com>
2024-03-14 19:32:41 +0100

    Fix missing Commandline

--- src/app.py
+++ src/app.py
@@ -12,96 +12,97 @@ sys.argv.pop(0)
 
 print(f"{sys.argv = }")
 
-match sys.argv[0]:
-    case "--download":
-        if sys.argv[-1].startswith("nxm://"):
-            from nxm_handler import handle
+if len(sys.argv):
+    match sys.argv[-1]:
+        case "--download":
+            if sys.argv[-1].startswith("nxm://"):
+                from nxm_handler import handle
 
-            handle()
-        else:
-            print("Missing Download URL!")
+                handle()
+            else:
+                print("Missing Download URL!")
 
-    case "--bind-nxm":
-        import winreg
+        case "--bind-nxm":
+            import winreg
 
-        from main import MainApp
+            from main import MainApp
 
-        REG_PATH = "nxm\\shell\\open\\command"
-        REG_VALUE = f'"{MainApp.executable}" --download "%1"'
+            REG_PATH = "nxm\\shell\\open\\command"
+            REG_VALUE = f'"{MainApp.executable}" --download "%1"'
 
-        try:
-            with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, REG_PATH) as hkey:
-                winreg.SetValue(hkey, "", winreg.REG_SZ, REG_VALUE)
+            try:
+                with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, REG_PATH) as hkey:
+                    winreg.SetValue(hkey, "", winreg.REG_SZ, REG_VALUE)
 
-            sys.exit(0)
+                sys.exit(0)
 
-        except OSError:
-            sys.exit(1)
+            except OSError:
+                sys.exit(1)
 
-    case "--show-docs":
-        from main import MainApp
+        case "--show-docs":
+            from main import MainApp
 
-        app = MainApp()
-        app.show_documentation_standalone()
-
-        sys.exit(0)
-
-    case "--extract-strings":
-        from pathlib import Path
-
-        plugin_path = Path(sys.argv[-1])
-
-        if not plugin_path.is_file():
-            print(f"{str(plugin_path)!r} is not an existing file!")
-            sys.exit(1)
-
-        if plugin_path.suffix.lower() in [".esp", ".esl", ".esm"]:
-            import utilities
-            from plugin_parser import PluginParser
-            import json
-
-            output_path = plugin_path.with_suffix(f"{plugin_path.suffix}.json")
-
-            parser = PluginParser(plugin_path)
-            strings = [
-                string.to_string_data()
-                for group in parser.extract_strings().values()
-                for string in group
-            ]
-
-            with output_path.open("w", encoding="utf8") as output_file:
-                json.dump(strings, output_file, indent=4, ensure_ascii=False)
-
-            print(f"Strings written to {str(output_path)!r}.")
+            app = MainApp()
+            app.show_documentation_standalone()
 
             sys.exit(0)
 
-    case "--help":
-        print(
-            "--download <NXM URL>                      Starts download of file at given URL"
-        )
-        print(
-            "--show-docs                               Opens Documentation without starting Main Application."
-        )
-        print(
-            "--bind-nxm                                Binds Mod Manager Downloads to SSE-AT. Mainly used for Admin Rights"
-        )
-        print(
-            "--extract-strings <Path to Plugin File>   Extracts Strings from Plugin and writes them in a JSON file with the same location and name."
-        )
+        case "--extract-strings":
+            from pathlib import Path
 
-        sys.exit(0)
+            plugin_path = Path(sys.argv[-1])
 
-    case "--version":
-        from main import MainApp
+            if not plugin_path.is_file():
+                print(f"{str(plugin_path)!r} is not an existing file!")
+                sys.exit(1)
 
-        print(f"{MainApp.name} v{MainApp.version} by Cutleast")
-        print()
-        print(
-            "Licensed under Attribution-NonCommercial-NoDerivatives 4.0 International"
-        )
+            if plugin_path.suffix.lower() in [".esp", ".esl", ".esm"]:
+                import utilities
+                from plugin_parser import PluginParser
+                import json
 
-        sys.exit(0)
+                output_path = plugin_path.with_suffix(f"{plugin_path.suffix}.json")
+
+                parser = PluginParser(plugin_path)
+                strings = [
+                    string.to_string_data()
+                    for group in parser.extract_strings().values()
+                    for string in group
+                ]
+
+                with output_path.open("w", encoding="utf8") as output_file:
+                    json.dump(strings, output_file, indent=4, ensure_ascii=False)
+
+                print(f"Strings written to {str(output_path)!r}.")
+
+                sys.exit(0)
+
+        case "--help":
+            print(
+                "--download <NXM URL>                      Starts download of file at given URL"
+            )
+            print(
+                "--show-docs                               Opens Documentation without starting Main Application."
+            )
+            print(
+                "--bind-nxm                                Binds Mod Manager Downloads to SSE-AT. Mainly used for Admin Rights"
+            )
+            print(
+                "--extract-strings <Path to Plugin File>   Extracts Strings from Plugin and writes them in a JSON file with the same location and name."
+            )
+
+            sys.exit(0)
+
+        case "--version":
+            from main import MainApp
+
+            print(f"{MainApp.name} v{MainApp.version} by Cutleast")
+            print()
+            print(
+                "Licensed under Attribution-NonCommercial-NoDerivatives 4.0 International"
+            )
+
+            sys.exit(0)
 
 from main import main
 
