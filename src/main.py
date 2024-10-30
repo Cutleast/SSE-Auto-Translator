6d4830de3bc5690b4230b557f6962521a015e8af
Cutleast <cutleast@gmail.com>
2024-10-30 19:29:12 +0100

    Increase App version to v2.1.5(-beta)

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ class MainApp(qtw.QApplication):
     """
 
     name = "SSE Auto Translator"
-    version = "2.1.4"
+    version = "2.1.5-beta"
 
     loc: "utils.Localisator" = None
     cur_path = (  # Get path of executable/script depending on building status
